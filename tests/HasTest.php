875f901583d5db1867debe7ad2c3c7ce4b0085cb
Sérgio Rafael Siqueira <sergio@inbep.com.br>
2016-09-16 14:21:00 -0300

    Add tests to has

---
+++ tests/HasTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Prelude\Tests;
+
+use function Prelude\has;
+
+class HasTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @test
+     */
+    public function shouldReturnTrue()
+    {
+        $hendrix = [
+            'username' => 'Hendrix',
+            'telephone' => '',
+            'year_old' => false,
+        ];
+
+        $this->assertTrue(has('username', $hendrix));
+        $this->assertTrue(has('telephone', $hendrix));
+        $this->assertTrue(has('year_old', $hendrix));
+    }
+
+    /**
+     * @test
+     */
+    public function shouldReturnFalse()
+    {
+        $this->assertFalse(has('telephone', []));
+        $this->assertFalse(has('telephone', ['telephone' => null]));
+    }
+}
