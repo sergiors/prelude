a795e3798365ee9b8ee5a994e97e72abbfc7ec5b
Sérgio Rafael Siqueira <sergio@inbep.com.br>
2016-09-22 15:18:02 -0300

    Add more tests

--- tests/PartialTest.php
+++ tests/PartialTest.php
@@ -15,9 +15,12 @@ class PartialTest extends \PHPUnit_Framework_TestCase
             return $a + $b + $c + $d;
         };
 
+        $result = partial($four, 1, 2, 3, 4);
+
         $this->assertInstanceOf(\Closure::class, partial($four));
         $this->assertInstanceOf(\Closure::class, partial($four)(1, 2));
         $this->assertInstanceOf(\Closure::class, partial($four)(1, 2));
         $this->assertEquals(10, partial($four)(1)(2)(3, 4));
+        $this->assertEquals(10, $result);
     }
 }
