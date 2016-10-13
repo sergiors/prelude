7d449993c0b0c0892fb7222d4198463fb7f45d67
Sérgio Rafael Siqueira <sergio@inbep.com.br>
2016-10-13 15:15:40 -0300

    Set optional ifElse value

--- src/ifElse.php
+++ src/ifElse.php
@@ -7,7 +7,7 @@ const ifElse = __NAMESPACE__.'\ifElse';
 function ifElse(...$args)
 {
     $fn = partial(function (callable $pred, callable $success, callable $fail) {
-        return function ($value) use ($pred, $success, $fail) {
+        return function ($value = null) use ($pred, $success, $fail) {
             return $pred($value)
                 ? $success($value)
                 : $fail($value);
