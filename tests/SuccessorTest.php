e4833739c7e8f2d5da44763bcc5adad669d24a06
Sérgio Rafael Siqueira <sergio@inbep.com.br>
2016-11-16 20:32:47 -0200

    Add test when don't call next callback

--- tests/SuccessorTest.php
+++ tests/SuccessorTest.php
@@ -36,4 +36,18 @@ class SuccessorTest extends \PHPUnit_Framework_TestCase
 
         $this->assertEquals('foo(bar(fizz))', $successor('fizz'));
     }
+
+    /**
+     * @test
+     */
+    public function shouldReturnJustFirstCallback()
+    {
+        $successor = successor(function ($x) {
+            return $x * 2;
+        }, function ($x) {
+            return $x + 10;
+        });
+
+        $this->assertEquals(10, $successor(5));
+    }
 }
