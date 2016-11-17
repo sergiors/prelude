c800a4396ed87e57f6c1f8bd5a6ef581034681ae
Sérgio Rafael Siqueira <sergio@inbep.com.br>
2016-11-17 03:50:21 -0200

    Rename var

--- src/successor.php
+++ src/successor.php
@@ -7,11 +7,11 @@ const successor = __NAMESPACE__.'\successor';
 function successor(callable ...$callbacks)
 {
     return function ($payload) use ($callbacks) {
-        $proxy = function ($payload) use ($callbacks) {
+        $next = function ($payload) use ($callbacks) {
             return call_user_func(successor(...tail($callbacks)), $payload);
         };
-        $fn = ifElse(isCallable, function (callable $callback) use ($payload, $proxy) {
-            return $callback($payload, $proxy);
+        $fn = ifElse(isCallable, function (callable $callback) use ($payload, $next) {
+            return $callback($payload, $next);
         }, always($payload));
 
         return $fn(get($callbacks, 0));
