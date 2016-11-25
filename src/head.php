8dfbb6cf2b10f8e8c1b645721ef6f4de969f67b5
Sérgio Rafael Siqueira <sergio@inbep.com.br>
2016-11-25 15:31:41 -0200

    Optimize head

--- src/head.php
+++ src/head.php
@@ -6,6 +6,9 @@ const head = __NAMESPACE__.'\head';
 
 function head(array $xss)
 {
-    $head = ifElse(equals([]), [Raise::class, 'invalid'], values);
-    return $head($xss)[0];
+    $head = ifElse(equals([]), [Raise::class, 'invalid'], function (array $xss) {
+        return array_values(array_slice($xss, 0, 1))[0];
+    });
+
+    return $head($xss);
 }
